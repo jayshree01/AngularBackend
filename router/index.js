fd8475932bf7b8a5c14b906c054763d33b41a248
jayshree01 <jayu9399@gmail.com>
2022-04-01 16:27:36 +0530

    jwt

--- router/index.js
+++ router/index.js
@@ -1,30 +1,48 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../model/user');
+const jwt = require('jsonwebtoken');
 // http://localhost:3000/singup
 // request:-{username: '',email: '',password: ''}
-router.post("/signin",(request,response)=>{
-   User.findOne({email: request.body.email,password: request.body.password})
-   .then(result=>{
-     if(result)
-      return response.status(200).json({status: 'loging success', current_user: result});
-     else
-      return response.status(200).json({status: 'login failed'}); 
-   })
-   .catch(err=>{
-     return response.status(500).json({error: 'Internal server error'});
-   });
+router.post("/signin", (request, response) => {
+  User.findOne({
+      email: request.body.email,
+      password: request.body.password
+    })
+    .then(result => {
+      if (result) {
+        let payload = {
+          subject: result._id
+        }
+        let token = jwt.sign(payload, 'fdssfsdfsdfsasddfas')
+        return response.status(200).json({
+          status: 'loging success',
+          current_user: result,
+          token: token
+        });
+      } else
+        return response.status(200).json({
+          status: 'login failed'
+        });
+    })
+    .catch(err => {
+      return response.status(500).json({
+        error: 'Internal server error'
+      });
+    });
 });
-router.post("/signup",(request,response)=>{
-   User.create({
-     username: request.body.username,
-     email: request.body.email,
-     password: request.body.password
-   }).then(result=>{
-     return response.status(201).json(result);
-   })
-   .catch(err=>{
-     return response.status(500).json({error: 'Internal Server Error'});
-   });
+router.post("/signup", (request, response) => {
+  User.create({
+      username: request.body.username,
+      email: request.body.email,
+      password: request.body.password
+    }).then(result => {
+      return response.status(201).json(result);
+    })
+    .catch(err => {
+      return response.status(500).json({
+        error: 'Internal Server Error'
+      });
+    });
 });
 module.exports = router;
